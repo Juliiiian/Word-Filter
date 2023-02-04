b8e13b69ead9aff7be3e98b6aa8a15852c5fc97d
Julian <Julian.Haussmann@gmx.de>
2023-02-04 18:53:45 +0100

    updated works now with args in console

--- main.js
+++ main.js
@@ -1,13 +1,37 @@
 const fs = require('fs').promises;
 const Filter = require('bad-words'),
 myFilter = new Filter();
- 
+
+const args = process.argv.slice(2);
+let inputFile = null;
+let outputFile = null;
+
+for (let i = 0; i < args.length; i++) {
+    switch (args[i]) {
+        case "-i":
+            inputFile = args[i + 1];
+            break;
+        case "-o":
+            outputFile = args[i + 1];
+            break;
+    }
+}
+
+if (!inputFile || !outputFile) {
+    console.error("Both input and output files must be specified");
+    process.exit(1);
+}
+
+inputFile = `./text/input/${inputFile}`;
+outputFile = `./text/output/${outputFile}`;
+
+console.log(`Input file: ${inputFile}`);
+console.log(`Output file: ${outputFile}`);
 
 async function main() {
-    const data = await fs.readFile("./input.txt", 'utf8');
+    const data = await fs.readFile(inputFile, 'utf8');
     let cleanedTxt = myFilter.clean(data);
-    console.log(cleanedTxt);
-    await fs.writeFile("output.txt", cleanedTxt); 
+    await fs.writeFile(outputFile, cleanedTxt);
 }
 
 main()
---
+++ readme.md
@@ -0,0 +1,3 @@
+This is a Word filter in Node.js. 
+
+Use with: node main.js -i test.txt -o testing.txt
